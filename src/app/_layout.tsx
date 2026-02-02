17e54efa58014edf49f8c9c1bed516f6d7ace96b
Yagnik <coderisaddicted@gmail.com>
2026-02-02 17:05:49 +0530

    Remove legacy starter assets, add Tailwind/Uniwind support

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,6 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import "../global.css";
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+	return <Stack />;
 }
